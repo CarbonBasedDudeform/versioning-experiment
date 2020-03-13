99741e940ac141b67d8d175755ccabbcd703b6b6
Dominic Musgrave <imdomdude@gmail.com>
2020-03-13 13:39:44 +0000

    Update SomeSingleFileLibrary.h
    tinkering to see what happens when you break do some breaking changes.

--- consoleapp/SomeSingleFileLibrary.h
+++ consoleapp/SomeSingleFileLibrary.h
@@ -4,6 +4,9 @@
 //users can access old versions via direct lib::v1::function()
 //or altnernatively, namespace lib = lib::v1
 //at root level, can just feed through to different versions of libraries
+//starts getting finicky when signatures start changing, which is more like real world changes
+//ie. v4 will probably require some new info that v1-3 didn't have
+//but can still maintain backwards compatibility cleanly
 
 namespace SomeSingleFileLibrary {
 	namespace v1 {
@@ -18,8 +21,18 @@ namespace SomeSingleFileLibrary {
 		}
 	}
 
+	namespace v3 {
+		struct dependency_injection_mode {
+			std::string ruler{ "Robots" };
+		};
 
-	std::string whoRunsTheWorld() {
-		return v2::whoRunsTheWorld();
+		std::string whoRunsTheWorld(const dependency_injection_mode& const mode) {
+			return mode.ruler;
+		}
+	}
+
+
+	std::string whoRunsTheWorld(const v3::dependency_injection_mode& const mode = {}) {
+		return v3::whoRunsTheWorld(mode);
 	}
 }
